6eebe2a281e9a21adfec823ebbe9e2fab34c9d54
Alunos --. Laboratório de T.I <aluno@lti.fatecso.edu.br>
2025-03-24 18:09:33 -0300

    Mais mudanças de CSS

--- obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
+++ obj/Debug/net9.0/TotemPWA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b559ae5a8b4ef08470906ad11ba3a329b54ac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f3692e083447c4c8bba31235d625e2d4fe1f85")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotemPWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
