0594f2bbd69805a5244838b30bae45806612e5b6
tutuca07segundo <arthurdoamaralbarreto@gmail.com>
2025-04-14 17:40:07 -0300

    preco unit sendo alterado

--- wwwroot/js/modalPersonalizar.js
+++ wwwroot/js/modalPersonalizar.js
@@ -19,9 +19,19 @@ function abrirModalPersonalizar(nome) {
     }
   }
   function adicionarItem(qnt,id){
-    
+    let precounit;
+    let preco;
     let atual = parseInt(document.getElementById(id).innerText);
+    if(precounit == null){
+      precounit = parseFloat(document.getElementById(id+'preco').innerText.replace('R$', ''));
+      precounit = precounit / atual;
+    }
+    // o ideal para evitar o erro de nan é usar o valor do banco depois no preco unitario
     atual += qnt;
+    
+    if(atual != 0){ preco = precounit * atual;}
+    else { preco = 0;}
+    document.getElementById(id+'preco').innerText = 'R$' + preco.toFixed(2);
     if(atual < 0) {atual = 0;}
     document.getElementById(id).innerHTML = atual;
 
