52978e1de474f402f8738ae41223999a27931f1c
tutuca07segundo <arthurdoamaralbarreto@gmail.com>
2025-06-16 16:12:50 -0300

    adicionando category controler e suas views

--- Controllers/Admin/CategoryController.cs
+++ Controllers/Admin/CategoryController.cs
@@ -3,13 +3,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using TotemPWA.Models;
 using TotemPWA.Data;
-//using TotemPWA.ViewModels;
+using TotemPWA.ViewModels;
 
 namespace TotemPWA.Controllers.Admin
 {
+    // /admin/category/list 
     [Route("Admin/[controller]/[action]")]
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var categories = _context.Categories
+                .Include(c => c.Subcategories)
+                .ToList();
+
+            return View(categories);
+        }
 
     }
 }
