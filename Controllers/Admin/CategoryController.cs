fb6046e86bdc1dddca09140e724242ee49a9ca12
tutuca07segundo <arthurdoamaralbarreto@gmail.com>
2025-06-23 15:26:41 -0300

    crud admin,carrinho,formatação telas

--- Controllers/Admin/CategoryController.cs
+++ Controllers/Admin/CategoryController.cs
@@ -27,6 +27,71 @@ namespace TotemPWA.Controllers.Admin
 
             return View(categories);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new CategoryViewModel
+            {
+                Categories = _context.Categories
+                    .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                    .ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(viewModel.Category);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            viewModel.Categories = _context.Categories
+                .Where(c => c.ParentCategoryId == null)
+                .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                .ToList();
+
+            return View(viewModel);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category == null) return NotFound();
+
+            var viewModel = new CategoryViewModel
+            {
+                Category = category,
+                Categories = _context.Categories
+                    .Where(c => c.Id != id && c.ParentCategoryId == null)
+                    .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                    .ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Edit(CategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(viewModel.Category);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            viewModel.Categories = _context.Categories
+                .Where(c => c.Id != viewModel.Category.Id)
+                .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                .ToList();
+
+            return View(viewModel);
+        }
 
     }
 }
