9d42fc410c3473642e4306dce4bba24249f8f6e5
Dark70941 <pedrohcd2007@gmail.com>
2025-06-29 21:34:57 -0300

    Crud ingredient

---
+++ Controllers/Admin/CupomController.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TotemPWA.Data;
+using TotemPWA.Models;
+
+namespace TotemPWA.Controllers.Admin
+{
+    public class CupomController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CupomController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var cupons = await _context.Cupons.ToListAsync();
+            return View(cupons);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Cupom cupom)
+        {
+            if (!ModelState.IsValid) return View(cupom);
+
+            _context.Cupons.Add(cupom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cupom = await _context.Cupons.FindAsync(id);
+            if (cupom == null) return NotFound();
+
+            return View(cupom);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Cupom cupom)
+        {
+            if (id != cupom.Id) return BadRequest();
+            if (!ModelState.IsValid) return View(cupom);
+
+            try
+            {
+                _context.Update(cupom);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Cupons.Any(c => c.Id == id))
+                    return NotFound();
+                throw;
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cupom = await _context.Cupons.FindAsync(id);
+            if (cupom == null) return NotFound();
+
+            return View(cupom);
+        }
+
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cupom = await _context.Cupons.FindAsync(id);
+            if (cupom == null) return NotFound();
+
+            _context.Cupons.Remove(cupom);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
---
+++ Controllers/Admin/IngredientController.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TotemPWA.Data;
+using TotemPWA.Models;
+
+namespace TotemPWA.Controllers.Admin
+{
+    [Route("admin/[controller]/[action]")]
+    public class IngredientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IngredientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /admin/Ingredient/List
+        public async Task<IActionResult> List()
+        {
+            var ingredients = await _context.Ingredients.ToListAsync();
+            return View(ingredients);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Ingredient ingredient)
+        {
+            if (!ModelState.IsValid) return View(ingredient);
+
+            _context.Ingredients.Add(ingredient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null) return NotFound();
+
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id) return BadRequest();
+            if (!ModelState.IsValid) return View(ingredient);
+
+            try
+            {
+                _context.Update(ingredient);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Ingredients.Any(i => i.Id == id))
+                    return NotFound();
+                throw;
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredient = await _context.Ingredients.FirstOrDefaultAsync(i => i.Id == id);
+            if (ingredient == null) return NotFound();
+
+            return View(ingredient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null) return NotFound();
+
+            _context.Ingredients.Remove(ingredient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
